cf52b896480924687aecd4ed1132688710fca203
Xavier Ortiz <joe.xavier.ortiz@gmail.com>
2015-11-02 23:19:23 -0500

    GREEN - established session with database.

---
+++ IntegraBusiness/src/main/java/com/xo/integrabusiness/persistence/DatabaseSession.java
@@ -0,0 +1,34 @@
+package com.xo.integrabusiness.persistence;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author admin
+ */
+class DatabaseSession {
+
+    
+    public DatabaseSession(){
+    
+    }
+    
+    public Session establishSession() {
+        Configuration configuration= new Configuration();
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        return session;
+    }
+    
+ 
+}
---
+++ IntegraBusiness/src/test/java/com/xo/integrabusiness/persistence/DatabaseSessionTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xo.integrabusiness.persistence;
+
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class DatabaseSessionTest {
+    
+    public DatabaseSessionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void createSessionWithDefaultParameters(){
+        DatabaseSession databaseSession = new DatabaseSession();
+        Session session = databaseSession.establishSession();
+        assertTrue(session.isConnected());
+    
+    }
+    
+}
